1a36cc44e300a880e2d72400cca77114a13ccf7f
Chris Patenaude <chris.patenaude@oregonstate.edu>
2024-02-03 13:48:32 -0800

    First commit

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-12">
+            <div className="flex place-items-center">
+                <p>
+                    This is the body of the about page.
+                </p>
+            </div>
+        </main>
+    );
+}
