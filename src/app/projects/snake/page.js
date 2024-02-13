0f9bd2277977b1b4d11d78aa0c21ddaf36ebbaf2
Chris Patenaude <chris.patenaude@oregonstate.edu>
2024-02-13 07:34:02 -0800

    Add empty /projects/snake

---
+++ src/app/projects/snake/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-12">
+            <div className="flex place-items-center">
+                <p>
+                    Ssssssssssssssssss.
+                </p>
+            </div>
+        </main>
+    );
+}
